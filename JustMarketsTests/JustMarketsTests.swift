ba08c788a33a74ab3d4a3e9019afa42911c03f5a
Mykhailo Zhutov <zhutov.misha@gmail.com>
2026-08-29 06:17:48 +0200

    Initial Commit

---
+++ JustMarketsTests/JustMarketsTests.swift
@@ -0,0 +1,19 @@
+//
+//  JustMarketsTests.swift
+//  JustMarketsTests
+//
+//  Created by Zhutov Mykhailo on 29.08.2026.
+//
+
+import Testing
+@testable import JustMarkets
+
+struct JustMarketsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
